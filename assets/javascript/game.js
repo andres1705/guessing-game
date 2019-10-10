a5c682020008cf1ea7fcb898e9f8c36f66d205f9
ANDRES <andres@ANDRESs-MacBook-Air.local>
2019-10-10 00:30:31 -0500

    game.js style.css updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,45 @@
-console.log('hello world');
+    var questions = [ 
+        { prompt: "What is the most common way to create a variable in JavaScript?(a) var (b) object
+                    (c) loop";
+
+        answer: "a"
+
+    };
+
+    { prompt: " What is the basic data structure in JvaScript? (a) loop (b) object (c) var";
+
+        answer: "b"
+
+    }:
+
+    { prompt: "What can you used to perform the same action mulmtiple times in a row? (a) loop (b) var (c) object";
+        answer: "a"
+    
+    };
+
+    var score = 0;
+
+    for(var i=0; i < questions.length; i++){
+        var response = window.prompt(questions[i].prompt)
+        if (response == questions[i].answer){
+            score++;
+            alert("correct");
+
+        } else {alert("wrong")}
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
